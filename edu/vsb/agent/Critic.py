4b24d4d1d44fbdc8b3778d79ea7c973ad7acae4d
vyomkeshj <vyomkeshjha@yahoo.com>
2020-08-21 22:45:22 +0200

    Before grayscale

--- edu/vsb/agent/Critic.py
+++ edu/vsb/agent/Critic.py
@@ -10,23 +10,33 @@ class Critic(nn.Module):
         # Q1 architecture
         self.l1 = nn.Linear(state_dim + action_dim, 500)
         self.l2 = nn.Linear(500, 256)
-        self.l3 = nn.Linear(256, 1)
+        self.l3 = nn.Linear(256, 256)
+        self.l4 = nn.Linear(256, 64)
+        self.l5 = nn.Linear(64, 1)
 
         # Q2 architecture
-        self.l4 = nn.Linear(state_dim + action_dim, 500)
-        self.l5 = nn.Linear(500, 256)
-        self.l6 = nn.Linear(256, 1)
+        self.l6 = nn.Linear(state_dim + action_dim, 500)
+        self.l7 = nn.Linear(500, 256)
+        self.l8 = nn.Linear(256, 256)
+        self.l9 = nn.Linear(256, 64)
+        self.l10 = nn.Linear(64, 1)
 
     def forward(self, state, action):
         sa = torch.cat([state, action], 1)
 
         q1 = F.relu(self.l1(sa))
         q1 = F.relu(self.l2(q1))
-        q1 = self.l3(q1)
+        q1 = F.relu(self.l3(q1))
+        q1 = F.relu(self.l4(q1))
 
-        q2 = F.relu(self.l4(sa))
-        q2 = F.relu(self.l5(q2))
-        q2 = self.l6(q2)
+        q1 = self.l5(q1)
+
+        q2 = F.relu(self.l6(sa))
+        q2 = F.relu(self.l7(q2))
+        q2 = F.relu(self.l8(q2))
+        q2 = F.relu(self.l9(q2))
+
+        q2 = self.l10(q2)
         return q1, q2
 
     def Q1(self, state, action):
@@ -34,5 +44,8 @@ class Critic(nn.Module):
 
         q1 = F.relu(self.l1(sa))
         q1 = F.relu(self.l2(q1))
-        q1 = self.l3(q1)
+        q1 = F.relu(self.l3(q1))
+        q1 = F.relu(self.l4(q1))
+
+        q1 = self.l5(q1)
         return q1
